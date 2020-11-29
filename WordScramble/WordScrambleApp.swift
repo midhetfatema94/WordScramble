f192dfaaedc9fd6bbccacb9a69268d11dd6f47d5
Midhet Sulemani <midhetfatema94@gmail.com>
2020-11-29 19:52:13 +0300

    first commit

---
+++ WordScramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by Waveline Media on 11/29/20.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
